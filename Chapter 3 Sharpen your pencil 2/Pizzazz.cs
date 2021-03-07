b94a6d93590e525e04fac7dc0e9a00d4b2aee8cd
Robert Hays <roberthays@hotmail.com>
2021-03-07 14:51:34 -0500

    Add project files.

---
+++ Chapter 3 Sharpen your pencil 2/Pizzazz.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpenYourPencil_2
+{
+    class Pizzazz
+    {
+        public int Zippo;
+
+        public void Bamboo(int eek)
+        {
+            Zippo += eek;
+        }
+    }
+
+}
